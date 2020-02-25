1beda9da847eb7b9802debb1a084e1eb42bb2590
sam <smuoki@healthcloud.co.ke>
2020-02-25 20:50:56 +0300

    Add EnterVehicleActivity

---
+++ app/src/main/java/com/example/zusha/EnterVehicleDetailsActivity.java
@@ -0,0 +1,44 @@
+package com.example.zusha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnterVehicleDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_vehicle_details);
+
+        Button enterVehicleDetailsButton = (Button) findViewById(R.id.enterVehicleDetailsButton);
+        enterVehicleDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText regNoEditText = (EditText) findViewById(R.id.regNoEditText);
+                EditText saccoEditText = (EditText) findViewById(R.id.saccoEditText);
+                EditText driverNameEditText = (EditText) findViewById(R.id.driverNameEditText);
+
+                String regNo = regNoEditText.getText().toString();
+                String sacco = saccoEditText.getText().toString();
+                String driver = driverNameEditText.getText().toString();
+
+                String regNoDetails = regNo;
+                String saccoDetails = sacco;
+                String driverDetails = driver;
+
+                Intent trackSpeed = new Intent(EnterVehicleDetailsActivity.this, Main2Activity.class);
+                trackSpeed.putExtra("regNoDetails", regNoDetails);
+                trackSpeed.putExtra("saccoDetails", saccoDetails);
+                trackSpeed.putExtra("driverDetails", driverDetails);
+                startActivity(trackSpeed);
+                finish();
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/zusha/MainActivity.java
+++ app/src/main/java/com/example/zusha/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,28 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button vehicleDetailsButton = (Button) findViewById(R.id.vehicleDetailsButton);
-        vehicleDetailsButton.setOnClickListener(new View.OnClickListener() {
+        Button trackSpeedButton = (Button) findViewById(R.id.trackSpeedButton);
+        trackSpeedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText regNoEditText = (EditText) findViewById(R.id.regNoEditText);
-                EditText saccoEditText = (EditText) findViewById(R.id.saccoEditText);
-                EditText driverNameEditText = (EditText) findViewById(R.id.driverNameEditText);
-
-                String regNo = regNoEditText.getText().toString();
-                String sacco = saccoEditText.getText().toString();
-                String driver = driverNameEditText.getText().toString();
-
-                String regNoDetails = regNo;
-                String saccoDetails = sacco;
-                String driverDetails = driver;
-
-                Intent i = new Intent(MainActivity.this, Main2Activity.class);
-                i.putExtra("regNoDetails", regNoDetails);
-                i.putExtra("saccoDetails", saccoDetails);
-                i.putExtra("driverDetails", driverDetails);
-                startActivity(i);
-                finish();
+                Intent enterVehicleDetails = new Intent(MainActivity.this, EnterVehicleDetailsActivity.class);
+                startActivity(enterVehicleDetails);
             }
         });
 
